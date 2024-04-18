b7c751f9789dc931fbfe2e8d0f5ef0ca12ade168
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-18 17:50:28 +0530

    third message

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+
+person1=Ramesh
+person2=Suresh 
+
+echo "$person1 :: Hello $person2, How are you?"
+echo "$person2 :: Hi $person1, I am doing good, What abt you?"
+echo "$person1 :: I am fine too, how is your work?"
+echo "S$person2 :: not bad, I am thinking to upgrade to Devops"
