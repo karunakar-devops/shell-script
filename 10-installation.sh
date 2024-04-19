0f97932794aa8f20013ce4b94164b907a0affc3d
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-19 15:38:42 +0530

    installation

--- 10-installation.sh
+++ 10-installation.sh
@@ -2,9 +2,9 @@
 
 USERID=($id -u)
  
- if [ $USERID -ne 0 ];
+ if [ $USERID -ne 0 ]
  then 
-    echo "need to run with root access"
+    echo "Please run  this script with root access"
 else
     echo "running with root user"
 fi
