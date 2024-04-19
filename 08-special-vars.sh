2f0131be512359900de09b8518c5d5bc50b81611
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-19 12:48:44 +0530

    special variables

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "All varaibles:$@"
+echo "No of varaibles Passed:$#"
+echo "current working directory:$PWD"
+echo "which user running this script:$USER"
+echo "Home directory of user:$HOME"
+echo "Hostname:$HOSTNAME"
+echo "scriptname:$0"
+echo "Process ID of the current shell script:$$"
+sleep 60&
+echo "Process ID of last background command:$!"
