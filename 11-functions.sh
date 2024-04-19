94d008aa99c0d1acb52edf45e2513cc96b3c9e01
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-19 18:54:36 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,8 +2,8 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT-NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT-NAME-$TIMESTAMP.log
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
 if [ $1 -ne 0 ]
