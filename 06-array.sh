b10caf22a2c5d7f4352a831a63b4abdcdb84e50a
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-19 12:07:56 +0530

    array variables

--- 06-array.sh
+++ 06-array.sh
@@ -4,4 +4,4 @@ MOVIES=("RRR" "Murari" "DJTillu")
 
 echo "First movie is:(${MOVIES[0], MOVIES[1]} 
 echo "second movie is:${MOVIES[1]}"
-echo "all movies are:${MOVIES[@]}"]}
+echo "all movies are:${MOVIES[@]}"
